1bf06b3ddc76f02bb61364599e19c93f36c0b5b3
roblescristian991 <roblescristian991@gmail.com>
2021-02-27 17:32:16 -0600

    crossplaform sleep

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 #include <chrono>
 #include <thread>
 #include "cursor.h"
-
+#if _WIN64 | _WIN32
+    #include <Windows.h>
+    #define SLEEP(x) Sleep(x);
+#else
+    #define SLEEP(x) std::this_thread::sleep_for(std::chrono::milliseconds(x));
+#endif
 
 int main() { 
     gotoxy(20,50);
@@ -17,7 +22,8 @@ int main() {
     using namespace std::chrono_literals;
     std::cout << "Hello waiter\n" << std::flush;
     auto start = std::chrono::high_resolution_clock::now();
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    SLEEP(2000)
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> elapsed = end-start;
     std::cout << "Waited " << elapsed.count() << " ms\n";
